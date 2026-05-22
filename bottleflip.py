808d3bd1727be2aca5cc8962590c8d7d5f41650b
SonyaDC7 <sonyacisse7@gmail.com>
2026-05-22 11:30:00 -0400

    Update bottleflip.py

--- bottleflip.py
+++ bottleflip.py
@@ -3,10 +3,13 @@ Web VPython 3.2
 from vpython import *
 
 
-bottle = cylinder(pos=vec(0, 0, 0), size=vec(1,1,1), axis=vec(0, 3, 0), color=color.white)
+bottle1 = cylinder(pos=vec(0, 0, 0), size=vec(1,1,1), axis=vec(0, 3, 0), color=color.white, opacity=0.5)
 
-#def com_ind(shape):
- #   x_cor = bottle.pos.x + bottle.size.x
+def com_ind(shape):
+    com = sphere(pos=vec(shape.pos + shape.axis/2), color=color.green, radius=0.1)
+    return com
+com_ind(bottle1)
+bottle2 = compound([bottle1, com_ind(bottle1)])
 
 def torque (force, lever_arm):
     return cross(force, lever_arm)
