a61d1373a3a24d9eb183c6bb12ab3a28bee88f2a
sgao2026 <sgao60@stuy.edu>
2026-05-26 15:20:00 -0400

    sliders for user input

--- bottleflip.py
+++ bottleflip.py
@@ -1,6 +1,6 @@
-Web VPython 3.2
+# Web VPython 3.2
 
-# from vpython import *
+from vpython import *
 
 Tapp = 0 # user determined torque applied
 bottle1 = cylinder(pos=vec(0, 0, 0), size=vec(1,1,1), axis=vec(0, 3, 0), color=color.white, opacity=0.5)
@@ -30,15 +30,31 @@ print(com_pts(masses, poses))
 def go(): # runs simulation
     return
 def setSliders(evt): # user inputted torque applied
+    global Tapp, percent_ice, init_height
     if evt.id == 'Tapp_slider':
         Tapp = evt.value
+        Tapp_label.text = '{:.2f}\n\n'.format(Tapp)
     elif evt.id == 'ice_slider':
         percent_ice = evt.value
+        ice_label.text = '{:.2f}\n\n'.format(percent_ice)
     elif evt.id == 'height_slider':
         init_height = evt.value
-#         print(Tapp)
-        
-run = button(bind=go(), text='Run')
+        height_label.text = '{:.2f}\n\n'.format(init_height)
+#     print(str(evt.id) + " " + str(Tapp))
+
+# sliders + labels
+run = button(bind=go(), text='Run', pos=scene.title_anchor)
+
+scene.caption = "Simulation Properties\n\n"
+
+scene.append_to_caption('Select applied torque\n')
 Tapp_slider = slider(id='Tapp_slider', bind=setSliders, min=0, value=Tapp, max=15, step=0.1, )
+Tapp_label = wtext(text='{:.2f}\n\n'.format(Tapp_slider.value))
+
+scene.append_to_caption('Select amount of ice\n')
 ice_slider = slider(id='ice_slider', bind=setSliders, min=0, value=percent_ice, max=1, step=0.01)
+ice_label = wtext(text='{:.2f}\n\n'.format(ice_slider.value))
+
+scene.append_to_caption('Select height of release\n')
 height_slider = slider(id='height_slider', bind=setSliders, min=0, value=init_height, max=5, step=0.1)
+height_label = wtext(text='{:.2f}\n\n'.format(height_slider.value))
