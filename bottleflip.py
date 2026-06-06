e467c92bb291ce98b789ee9f9044d7a1640b7a52
Sandra Gao <sgao60@stuy.edu>
2026-06-05 20:45:56 -0400

    checking upright + win/lose screens

--- bottleflip.py
+++ bottleflip.py
@@ -4,6 +4,15 @@ first = vec(1,0,0)
 second = vec(0,1,0)
 third = vec(0,0,1)
 
+bottom_right_back = 0
+bottom_right_front = 1
+bottom_left_back = 2
+bottom_left_front = 3
+top_right_back = 4
+top_right_front = 5
+top_left_back = 6
+top_left_front = 7
+
 bottle_mass = 10 # g
 bottle_radius = 3.175 # cm
 bottle_length = 20.3 # cm
@@ -13,17 +22,18 @@ g = -1000 # cm/s^2
 
 scene.width = 700
 scene.height = 500
+scene.userzoom = False
 scene.camera.pos = vec(0,bottle_length * 2,100)
-table = box(pos=vec(0, -0.2, 0), length=scene.width, height=0.1, width=10, color=color.white)
 
 t = 0.00
-dt = 0.005 # always increment time by 0.01
+dt = 0.001
 flips = 0
 trail = []
 
-Fapp = vec(-1700000,0,0) # user determined torque applied
+Fapp = vec(-4163000,0,0) # user determined torque applied
 percent_ice = 0.5 # percentage of total volume of the bottle
 init_pos = vec(40,0,0) # coordinate of bottle
+release_angle = pi/2
 
 wrist = sphere(pos=init_pos + vec(0,bottle_length + 7,0), radius = 0.5)
 origin = sphere(color=color.yellow)
@@ -33,6 +43,9 @@ def orth(u): # rotate pi/2 ccw
 
 def vol(shape): # cylinders only
     return pi * shape.radius**2 * shape.length
+
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
 def torque (lever_arm, force):
     return cross(lever_arm, force)
@@ -79,6 +92,9 @@ init_tvel = bottle_ice.tvel
 init_avel = bottle_ice.avel
 init_theta = bottle_ice.theta
 
+# win/lose screen
+win = text(pos=vec(-50,50,0), height=10, text='Success!', align='center', color=color.green, visible=False)
+lose = text(pos=vec(-50,50,0), height=10, text='Fail!', align='center', color=color.red, visible=False)
 
 def get_contact_points():
     result = []
@@ -96,8 +112,12 @@ def full_contact(ground):
     
     return contact_count == 4
 
+def check_upright():
+    print(dot(hat(bottle_ice.axis), second))
+    return isclose(dot(hat(bottle_ice.axis), second), 1, abs_tol=0.0001)
+
 def setup():
-    global bottle_ice, flips, t
+    global bottle_ice, flips, t, win
     
     # reset everything
     bottle_ice.pos = init_pos
@@ -107,14 +127,16 @@ def setup():
     bottle_ice.theta = init_theta
     flips = 0
     t = 0
-
-    finished.visible = False
     
     # clear trail
     for p in trail:
         p.visible = False
         p = None
     trail.clear()
+    
+    # hide win screen
+    win.visible = False
+    lose.visible = False
     
 def draw_parabola (v_initial,  a_y = g):
     global t, bottle_ice, bottle_ice_com, flips
@@ -145,8 +167,7 @@ def draw_parabola (v_initial,  a_y = g):
         
         xDots.plot(t,x)
         yDots.plot(t,y)
-        tkDots.plot(t, 0.5 * (ice_mass + bottle_mass) * (v_x**2 + v_y**2))
-        
+        tkDots.plot(t, 0.5 * (ice_mass + bottle_mass) * (v_x**2 + v_y**2))        
         
         t = t + dt
         
@@ -167,7 +188,7 @@ def flip():
     I_bottle = 0.5 * bottle_mass * bottle_radius**2 + 1/12 * bottle_mass * bottle_length**2 + bottle_mass * mag(bottle_ice.group_to_world(bottle_com.pos) - wrist.pos)**2
     I_ice = 0.25 * ice_mass * ice.radius**2 + 1/12 * ice_mass * ice.length**2 + ice_mass * mag(bottle_ice.group_to_world(ice_com.pos) - wrist.pos)**2
     
-    while bottle_ice.theta <= pi/2:
+    while bottle_ice.theta <= release_angle:
         rate(1 / dt)
         
         lever_g = bottle_ice.group_to_world(bottle_ice_com.pos) - wrist.pos
@@ -226,12 +247,14 @@ def impact():
         akDots.plot(t, 0.5 * (I_bottle + I_ice) * bottle_ice.avel**2)
         
         t = t + dt
+        
+        # trail
+        trail.append(sphere(pos=bottle_ice.pos, color=ice.color))
     return
-
-finished = label(pos=vec(-15, 15, 0), text='Flip done!', xoffset=20, yoffset=50, space=30, height=16, font='Helvetica', line = False, background = color.green)
-
+    
 def go(): # runs simulation
-    global bottle_ice, run
+    global bottle_ice, run, win, lose
+    if (mag(Fapp) == 0): return
     run.text = 'Pause'
     
     # flip
@@ -239,23 +262,29 @@ def go(): # runs simulation
     
     # parabola
     draw_parabola(bottle_ice.tvel)
-
-    finished.visible = True
     
     # impact behavior
     impact()
-    print("impact done")
+    
+    # check upright
+    if (check_upright()):
+        win.visible = True
+    else:
+        lose.visible = True
     
     run.text = 'Run'
     return
 
 def setSliders(evt): # user inputs
-    global Fapp, Farrow, percent_ice, init_height, ice, ice_com, bottle, bottle_ice, bottle_ice_com, init_pos, o
+    global Fapp, Farrow, percent_ice, init_height, ice, ice_com, bottle, bottle_ice, bottle_ice_com, init_pos, wrist, release_angle
     if evt.id == 'Fapp_slider':
         Fapp = vec(-1 * evt.value,0,0)
         Fapp_label.text = '{:.2f} kN\n\n'.format(Fapp_slider.value * 10**-5)
         
-        Farrow.axis = hat(Farrow.axis) * mag(Fapp)*10**-5
+        if (mag(Farrow.axis) == 0):
+            Farrow.axis = hat(orth(bottle_ice.axis)) * mag(Fapp)*10**-5
+        else:
+            Farrow.axis = hat(Farrow.axis) * mag(Fapp)*10**-5
     elif evt.id == 'ice_slider':
         percent_ice = evt.value
         ice_label.text = '{:.0f}%\n\n'.format(percent_ice * 100)
@@ -270,10 +299,13 @@ def setSliders(evt): # user inputs
         height_label.text = '{:.2f} cm\n\n'.format(init_height)
         
         init_pos = init_pos + vec(0,delta_h,0)
+        wrist.pos = wrist.pos + vec(0,delta_h,0)
         bottle_ice.pos = bottle_ice.pos + vec(0,delta_h,0)
         
         init_height = evt.value
-#     print(str(evt.id) + " " + str(Tapp))
+    elif evt.id == 'angle_slider':
+        release_angle = evt.value
+        angle_label.text = '{:.2f} radians\n\n'.format(angle_slider.value)#     print(str(evt.id) + " " + str(Tapp))
 
 # sliders + labels
 reset = button(bind=setup, text='Reset', pos=scene.title_anchor)
@@ -295,6 +327,10 @@ ice_label = wtext(text='{:.0f}%\n\n'.format(ice_slider.value * 100))
 scene.append_to_caption('Select height of release\n')
 height_slider = slider(id='height_slider', bind=setSliders, min=0, value=dot(init_pos,second), max=bottle_length * 2, step=0.1)
 height_label = wtext(text='{:.2f} cm\n\n'.format(height_slider.value))
+
+scene.append_to_caption('Select angle of release\n')
+angle_slider = slider(id='angle_slider', bind=setSliders, min=0, value=release_angle, max=pi/2, step=0.1)
+angle_label = wtext(text='{:.2f} radians\n\n'.format(angle_slider.value))
 
 # graphs
 x_t = graph(width=350, height=250, xtitle=("Time"), ytitle=("X Position"), align='left')
